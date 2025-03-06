6de56ba02c493dd9c2856f648fb23a87407c8775
unknown <jandyroachy@gmail.com>
2025-03-06 10:16:15 +0530

    completed

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,26 @@
 // write the component code here
+import React from "react";
+
+
+const Usercard = () => {
+  // Static user details
+  const profilePhoto = "./src/assets/myphoto.jpg";
+  const name = "Jandy Roach";
+  const email = "jandyroachy@gmail.com";
+  const phone = "1234567890";
+  const address = "1234 Main St, City, State, 12345";
+
+  return (
+    <div className="profile-info">
+      <img
+        src={profilePhoto}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+      <p>{phone}</p>
+      <p>{address}</p>
+    </div>
+  );
+};
+
+export default Usercard;
